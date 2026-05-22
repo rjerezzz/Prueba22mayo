5141b62e8703e397585cd02477f2fe9f329fa01b
rjerezzz <robjerez@uamv.edu.ni>
2026-05-22 09:27:19 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*Integrante 1 va a crear los arreglos y definir las variables. Requerimiento 2*/
+
+
+
+
+/*Integrante 2 va a rellenar la info y validar las entradas. Requerimiento 1 */
+
+
+
+
+
+
+/* Integrante 3 va a realizar los calculos y las estadisticas. Requerimiento 4, 6*/
+
+
+
+
+
+/* Integrante 4 va a mostrar los resultados y corregir errores. Requerimientos 3, 5*/
+
+
+
+
+
