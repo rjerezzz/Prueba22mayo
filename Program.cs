3391904dab8186c1eb51e38c2a764420a602d23c
pjjgomez-cmd <pjjgomez@uamv.edu.ni>
2026-05-22 10:38:00 -0600

    feat: Codigo del integrante 3

--- Program.cs
+++ Program.cs
@@ -3,48 +3,6 @@ int[] cantidad = new int[15];
 double[] precio = new double[15];
 double suma = 0;
 
-bool salir = false;
-
-do
-{
-    Console.WriteLine("===== MENÚ =====");
-    Console.WriteLine("1. Registrar productos");
-    Console.WriteLine("2. Mostrar productos");
-    Console.WriteLine("3. Calcular total vendido");
-    Console.WriteLine("4. Mostrar producto más vendido");
-    Console.WriteLine("5. Mostrar promedio de ventas");
-    Console.WriteLine("6. Salir");
-
-    Console.Write("Ingrese una opción: ");
-    int.TryParse(Console.ReadLine(), out int opcion);
-
-    switch (opcion)
-    {
-        case 1:
-            for (int i = 0; i < productos.Length; i++)
-            {
-                {
-                    try
-                    {
-                        Console.WriteLine($"Ingrese el nombre del producto {i + 1}: ");
-                        productos[i] = Console.ReadLine();
-
-                        Console.WriteLine($"Ingrese la cantidad del producto {i + 1}: ");
-                        cantidad[i] = int.Parse(Console.ReadLine());
-
-                        Console.WriteLine($"Ingrese el precio del producto {i + 1}: ");
-                        precio[i] = double.Parse(Console.ReadLine());
-                    }
-                    catch
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Entrada inválida. Por favor, ingrese un valor correcto.");
-                        Console.ResetColor();
-                        i--;
-                    }
-                }
-            }
-            break;
         case 2:
             // Código para mostrar productos
             for(int i = 0; i < productos.Length; i++)
